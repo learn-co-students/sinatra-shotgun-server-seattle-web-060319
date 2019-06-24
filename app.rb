177d784c74487fb669ad88fb903884ac76bec991
Dex Fitch <fitch.dexter@gmail.com>
2019-06-24 16:24:44 -0700

    bang bang

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "All I wanna do is SHOTGUN SHOTGUN SHOTGUN SHOTGUN ka-ching"
   end
 
 end
